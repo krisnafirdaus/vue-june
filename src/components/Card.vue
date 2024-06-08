91ea89d07541966e83de21b45f564a53c206ddb8
Krisna Firdaus <krisnafirdaus@gmail.com>
2024-06-08 14:22:36 +0700

    initial

---
+++ src/components/Card.vue
@@ -0,0 +1,42 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+
+const data = [
+  {
+    id: 1,
+    title: "Card 1",
+    content: "This is card 1",
+  },
+  {
+    id: 2,
+    title: "Card 2",
+    content: "This is card 2",
+  },
+  {
+    id: 3,
+    title: "Card 3",
+    content: "This is card 3",
+  },
+];
+</script>
+
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+		<div v-for="item in data" :key="item.id">
+			<h2>{{ item.title }}</h2>
+			<p>{{ item.content }}</p>
+		</div>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  color: red;
+}
+</style>
